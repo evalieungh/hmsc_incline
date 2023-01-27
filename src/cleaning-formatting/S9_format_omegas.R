c67f371c9d1c5e52566ee2c135dffbb15beffea8
Eva Lieungh <39821980+evalieungh@users.noreply.github.com>
2023-01-27 09:30:45 +0100

    ordination and microclimate updates

--- src/cleaning-formatting/S9_format_omegas.R
+++ src/cleaning-formatting/S9_format_omegas.R
@@ -1,19 +1,21 @@
-###################################
-#    format omega output matrix   #
-###################################
+################################
+#  format omega output matrix  #
+################################
 
 # script by Eva Lieungh
 
 library(tidyverse)
 
-# Omegas stored from OO's Hmsc pipeline as .xlsx with associations, posterior probabilities of omega>0 and <0 (sum to 1). Manually saved the omegas and positive probabilities as .csv files. 
+# Omegas stored from OO's Hmsc pipeline as .xlsx with associations, 
+# posterior probabilities of omega>0 and <0 (sum to 1). 
+# Manually saved the omegas and positive probabilities as .csv files
 
 # read data matrix (a=association=omega estimate)
 #-------------------------------------
-a_skj <- read.csv('data/model_output/Omega_Skjellingahaugen_subplot.csv', row.names = 1)
-a_gud <- read.csv('data/model_output/Omega_Gudmedalen_subplot.csv',row.names = 1) # estimated associations
-a_lav <- read.csv('data/model_output/Omega_Lavisdalen_subplot.csv',row.names = 1)
-a_ulv <- read.csv('data/model_output/Omega_Ulvehaugen_subplot.csv',row.names = 1)
+a_skj <- read.csv('../../data_processed/model_output/Omega_Skjellingahaugen_subplot.csv', row.names = 1)
+a_gud <- read.csv('../../data_processed/model_output/Omega_Gudmedalen_subplot.csv',row.names = 1) # estimated associations
+a_lav <- read.csv('../../data_processed/model_output/Omega_Lavisdalen_subplot.csv',row.names = 1)
+a_ulv <- read.csv('../../data_processed/model_output/Omega_Ulvehaugen_subplot.csv',row.names = 1)
 
 # collect all data as list of association objects
 a_obj = list('a_skj'=a_skj,
@@ -22,7 +24,7 @@ a_obj = list('a_skj'=a_skj,
              'a_ulv'=a_ulv)
 
 # save a_obj list of dataframes for use in other scripts, full matrices
-saveRDS(a_obj,file = 'data/a_obj_full.RData')
+saveRDS(a_obj,file = '../../data_processed/a_obj_full.Rds')
 
 # set upper triangle of matrices to NA, including the diagonal
 for (i in 1:4) {
@@ -31,12 +33,35 @@ for (i in 1:4) {
 length(which(is.na(a_obj[[3]]))) # number of NAs in half matrix should be (58*57)/2+58=1711
 
 # save a_obj list of dataframes for use in other scripts, lower half of matrix
-saveRDS(a_obj,file = 'data/a_obj_lower.RData')
+saveRDS(a_obj,file = '../../data_processed/a_obj_lower.Rds')
 
-# format for plotting
-# -----------------------------------
+# pivot longer 
+# --------------------------------------
+# ...to get species pairs as columns and their co-occurrence as third axis
+a_obj <- readRDS('../../data_processed/a_obj_lower.Rds')
+
+# collect all omega matrices for all 4 sites
+omegas <- data.frame(siteID = c(rep('Gudmedalen',58),rep('Lavisdalen',58),
+                                rep('Skjellingahaugen',58),rep('Ulvehaugen',58)),
+                     speciesA = rep(rownames(a_obj[[1]]),4),
+                     rbind(a_obj[[1]],a_obj[[2]],a_obj[[3]],a_obj[[4]]))
+
+# pivot longer to get sites, species pairs (A and B), and omega value
+omegas_l <- omegas %>%
+  pivot_longer(Ach_mil:Vio_pal, # all species, but not site or species name columns
+               names_to = 'speciesB',
+               values_to = 'omega')
+
+# remove rows where omega is NA
+omegas_l <- na.omit(omegas_l)
+
+# save object
+saveRDS(omegas_l,'../../data_processed/omegas_long.Rds')
+
+# sum co-occurrences per species and pivot long
+# ------------------------------------------------
 # read if necessary
-Mfull <- readRDS('data/a_obj_full.RData')
+Mfull <- readRDS('../../data_processed/a_obj_full.RData')
 
 # test with only Skjellingahaugen omegas first:
 # remove diagonal (set to NA)
@@ -75,7 +100,7 @@ df <- df %>%
   mutate(assoc_strength = ifelse(association > 0.33, 'pos',
                                  ifelse(association < -0.33, 'neg', 'weak')))
 # save object
-saveRDS(df,'data/plotting_df.RData')
+saveRDS(df,'data_processed/omegas_species_sums.RData')
 
 # subsetting
 # -----------------------------------
